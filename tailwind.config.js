ad39f5bc845a36d236e1824d47e622ed1f2310df
Wai Yan Hein <wyan00196@gmail.com>
2024-02-23 21:35:51 +0630

    ui-design-start

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
